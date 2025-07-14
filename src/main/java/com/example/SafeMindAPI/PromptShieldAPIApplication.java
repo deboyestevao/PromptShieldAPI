be3f1bf05ed279c8a5c0d73b69affa6197d0bfa2
deboyestevao <joaopedrotuber2004@gmail.com>
2025-07-14 15:00:30 +0100

    Correção de alteração do nome

--- src/main/java/com/example/SafeMindAPI/PromptShieldAPIApplication.java
+++ src/main/java/com/example/SafeMindAPI/PromptShieldAPIApplication.java
@@ -2,9 +2,11 @@ package com.example.SafeMindAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
-@EnableConfigService
+@EnableConfigServer
 public class PromptShieldAPIApplication {
 
 	public static void main(String[] args) {
