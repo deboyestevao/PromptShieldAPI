f39e5e994fa3a4564ed82b61fda69df6a67036f9
deboyestevao <joaopedrotuber2004@gmail.com>
2025-07-23 17:00:54 +0100

    Problemas com o login no postman, na web está tudo bem

---
+++ src/main/java/com/example/PromptShieldAPI/controller/AuthControllerWeb.java
@@ -0,0 +1,22 @@
+package com.example.PromptShieldAPI.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthControllerWeb {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/auth/login";
+    }
+}
